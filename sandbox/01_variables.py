fc5f34ae0795e79a49795235e435b4a5bc8a405c
Rodrigocrispim <rodrigocrispim015@gmail.com>
2026-02-13 14:46:21 +0000

    Variables

---
+++ sandbox/01_variables.py
@@ -0,0 +1,10 @@
+# declaração de variáveis
+apartamento_t2 = 155250
+rating_investimento = 4.5
+precisa_obras = True
+proprietario = "Mário da Silva"
+
+# output
+print("-----Análise de Mercado-----")
+print(f"O imóvel do {proprietario} custa {apartamento_t2}€")
+print(f"Avaliação do investimento = {rating_investimento}")
